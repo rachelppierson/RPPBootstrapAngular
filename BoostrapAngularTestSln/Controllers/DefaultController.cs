bd02981c4d4d58595aaae05397e68fc5365d2035
EU\PiersonRx <rachel.pierson@hotmail.co.uk>
2015-01-15 22:22:37 +0000

    Angular / Bootstrap demo project

---
+++ BoostrapAngularTestSln/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using AttributeRouting.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoostrapAngularTestSln.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public DefaultController()
+        {
+
+        }
+
+        [GET("RPPTest")]
+        public ActionResult GetRPPTest()
+        {
+            return View();
+        }
+	}
+}
---
+++ BoostrapAngularTestSln/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BoostrapAngularTestSln.MvcApplication" Language="C#" %>
